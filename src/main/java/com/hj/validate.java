eecfa321289a0b53a17294119b30f439a989f37c
hj18756057535 <956126200@qq.com>
2020-05-24 17:46:44 +0800

    add: add mvc custom validate

---
+++ src/main/java/com/hj/validate.java
@@ -0,0 +1,17 @@
+package com.hj;
+
+/**
+ * @author Administrator
+ */
+public class validate {
+    public void validate(){
+//        spring mvc use custom bean property bind validate
+//        UserFlowTaskInputVO userFlowTaskInputVO = new UserFlowTaskInputVO();
+//        userFlowTaskInputVO.setProjectId(channelId);
+//        BindingResult result = new BeanPropertyBindingResult(userFlowTaskInputVO, "userFlowTaskInputVO");
+//        userFlowTaskValidator.validate(userFlowTaskInputVO, result);
+//        if (result.hasErrors()) {
+//            return result;
+//        }
+    }
+}
